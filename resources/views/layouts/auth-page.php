70cc3e713d27db7a1541eaa95e02078b2a42dea1
CharmTzy <wai71308@gmail.com>
2026-03-28 02:25:11 +0800

    Split admin and auth stylesheets

--- resources/views/layouts/auth-page.php
+++ resources/views/layouts/auth-page.php
@@ -41,6 +41,7 @@ if ($robotsMeta !== '' && !headers_sent()) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="<?= e(asset('css/app.css')) ?>">
+    <link rel="stylesheet" href="<?= e(asset('css/auth.css')) ?>">
 </head>
 
 <body class="<?= e($bodyClass) ?>">
--- resources/views/layouts/header.php
+++ resources/views/layouts/header.php
@@ -147,6 +147,9 @@ if ($robotsMeta !== '' && !headers_sent()) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="<?= e(asset('css/app.css')) ?>">
+    <?php if ($isAdminArea): ?>
+        <link rel="stylesheet" href="<?= e(asset('css/admin.css')) ?>">
+    <?php endif; ?>
 </head>
 
 <body class="<?= e($bodyClasses) ?>">
