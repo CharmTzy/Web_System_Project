de68f51b8595dcc07e5014c923444d8e03f98618
CharmTzy <wai71308@gmail.com>
2026-03-28 22:23:22 +0800

    Align page skeletons with real layouts

--- public/seller/chat.php
+++ public/seller/chat.php
@@ -39,7 +39,7 @@ try {
 $pageTitle = 'Seller Chat';
 $appName = $config['app']['name'];
 $pageScript = 'chat.js';
-$pageSkeletonVariant = 'panel';
+$pageSkeletonVariant = 'chat';
 $cartSummary = ['total_items' => 0];
 $chatWebSocketUrl = chat_websocket_url($config['app']);
 
